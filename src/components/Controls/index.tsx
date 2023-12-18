98679729df57979d65c00c1cacf31be365f02ab4
Linda Ngo <lingo7831@gmail.com>
2023-12-17 22:29:27 -0500

    autoplay

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,19 +1,32 @@
 import SpotifyPlayer from "react-spotify-web-playback";
 import { IState } from "../../types";
 import { IProps } from "../../interfaces";
+import { useState } from "react";
 
 const Controls = ({token, tracks, setTrack}: IProps) => {
+    
+    /**
+     * Boolean state play, that indicates if somethin is played.
+     * Controls playblack. 
+     * Allows autoplay when we click on a playlist.
+     */
+    const [play, setPlay] = useState(true);
+    
     return (
         <>
             <SpotifyPlayer
                 token={token}
+                play={play}
                 uris={tracks}
                 hideCoverArt={true}
                 hideAttribution={true}
                 inlineVolume={false}
                 callback={ (state: IState) => {
                     // Local State
-                    setTrack(state.track)
+                    setTrack(state.track);
+                    if (state.isPlaying) {
+                        setPlay(true);
+                    }
                 }}
             />
         </>
