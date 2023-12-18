0bc57837bd9d71b95cdeeb332c946145f8f3ec51
Linda Ngo <lingo7831@gmail.com>
2023-12-18 00:34:49 -0500

    update uri

--- src/auth.ts
+++ src/auth.ts
@@ -13,7 +13,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://master--statuesque-kringle-280673.netlify.app/callback");
     params.append("scope", scope);
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
